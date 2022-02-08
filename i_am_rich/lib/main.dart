a1d40680e44658c4e5cd10ce43804872cf3d3b52
Alex Wong <alex.wong@orangetesting.com>
2022-02-08 15:25:58 -0300

    Chapter 2 on Part 2 - Done - NetworkImage!

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false, // Removes Debug Banner
+      home: Scaffold(
+        appBar: AppBar(
+            title: Center(
+              child: Text('I Am Richer'),
+            ),
+            backgroundColor: Colors.blueGrey[900]),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://www.w3schools.com/w3css/img_lights.jpg'),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
