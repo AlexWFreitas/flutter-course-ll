dd4b2eb9ee08fcfa9c9bb0bc8bd0d1f9e93e0689
Alex Wong <alex.wong@orangetesting.com>
2022-02-10 17:54:07 -0300

    Part 3 - Container, EdgeInsets, SafeArea

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -10,9 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            child: Text('Hello'),
+            color: Colors.white,
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(
+              left: 30.0,
+            ),
+            padding: EdgeInsets.only(
+              left: 20.0,
+              top: 20.0,
+            ),
+          ),
+        ),
       ),
     );
   }
