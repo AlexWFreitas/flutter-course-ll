021f17a5a208e886897679cfdcde4f75d9022129
Alex Wong <alex.wong@orangetesting.com>
2022-02-16 11:40:03 -0300

    Part 4 - Chapter 2 - Starting Project for Dicee, Image.asset, Expanded, flex property, Custom Classes / Widgets

---
+++ dicee_flutter/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Dicee'),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset(
+            'images/dice2.png',
+          ),
+        ),
+      ],
+    );
+  }
+}
