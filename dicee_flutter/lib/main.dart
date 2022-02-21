ac3a7f22ff8ceeaf5147797e90a642da53f559b1
Alex Wong <alex.wong@orangetesting.com>
2022-02-21 11:04:03 -0300

    Part 4 - Complete - dart:math, Random class, Dicee Challenges completed, Magic 8 Ball Flutter completed

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -18,13 +20,26 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    var leftDiceNumber = 5;
+  _DicePageState createState() => _DicePageState();
+}
 
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
@@ -34,7 +49,7 @@ class DicePage extends StatelessWidget {
               child: TextButton(
                 onPressed: () {
                   if (kDebugMode) {
-                    print('Left button got pressed');
+                    changeDiceFace();
                   }
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -47,11 +62,11 @@ class DicePage extends StatelessWidget {
               child: TextButton(
                 onPressed: () {
                   if (kDebugMode) {
-                    print('Right button got pressed');
+                    changeDiceFace();
                   }
                 },
                 child: Image.asset(
-                  'images/dice2.png',
+                  'images/dice$rightDiceNumber.png',
                 ),
               ),
             ),
